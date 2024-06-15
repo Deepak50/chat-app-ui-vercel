277e8f082008ebf56842c8f0cfca606148d6ead6
Kushalappa C A <kushalappa.ca@Incture.local>
2024-06-15 23:17:40 +0530

    changed ui and backend endpoint to deployed url

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
-export const UI_END_PT = "http://localhost:3000";
-export const BACKEND_END_PT = "http://localhost:8080";
+export const UI_END_PT = "https://chat-app-ui-vercel1.vercel.app";
+export const BACKEND_END_PT = "https://chatapp-zoa1.onrender.com";
 
 export const CLIENT_ID = "49280116826-5j8ia9avme7fhj4eu456mvqlafm3qmss.apps.googleusercontent.com";
 export const CLIENT_SECRET = "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";
@@ -9,8 +9,6 @@ export const REDIRECT_URI = UI_END_PT;
 
 export const GOOGLE_TOKEN_END_PT = "https://oauth2.googleapis.com/token";
 export const GOOGLE_AUTHCODE_END_POINT = "https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&access_type=offline&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri="+UI_END_PT+"&client_id=49280116826-5j8ia9avme7fhj4eu456mvqlafm3qmss.apps.googleusercontent.com";
-
-
 
 // remote-ui: https://chat-app-ui-vercel1.vercel.app
 // remote-backend: https://chatapp-zoa1.onrender.com
