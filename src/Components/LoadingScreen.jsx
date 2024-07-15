6ca6f8804e5a62c39855caa0cd1d9f531e400aa0
INCTURE0\kushalappa.ca <kushalappa.ca@incture.com>
2024-07-15 15:25:21 +0530

    changed loading screen message

--- src/Components/LoadingScreen.jsx
+++ src/Components/LoadingScreen.jsx
@@ -1,7 +1,27 @@
-function LoadingScreen(){
-    return(
+function LoadingScreen() {
+    return (
         <div>
-            Since we are using a free, server. The server spins down after 15 mins of inactivity. Please wait for 3 mins while we spin up the server.
+            <div>
+                <b>Since we are using a free server. The server spins down after 15 mins of inactivity. Please wait for 3 mins while we spin up the server (This is a one time process).</b>
+
+
+
+            </div>
+
+            <p>
+                <b>Note:</b>
+                <br></br>
+                If you are a first time user, you might not have anybody on your chatlist. You have to add another friend who is registered with the app.
+                <br></br>
+                <br></br>
+                You can use these emails as your first friends:
+                <ul>
+                    <li>deepakkushalappa123@gmail.com</li>
+                    <li>deepakkushalappa50@gmail.com</li>
+                    <li>deepakkushalappa321@gmail.com</li>
+                    <li>dca777@gmail.com</li>
+                </ul>
+            </p>
         </div>
     )
 }
